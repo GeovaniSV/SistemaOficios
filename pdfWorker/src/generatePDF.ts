fd5136c2e98a5f33347a154c4e344a832f4a7a59
Geovani Dos Santos Vargas <geovani.020204@gmail.com>
2026-06-18 01:03:30 -0400

    fixing oficio number

--- pdfWorker/src/generatePDF.ts
+++ pdfWorker/src/generatePDF.ts
@@ -154,10 +154,10 @@ export async function generatePDF(data: string) {
 
     content: [
       {
-        text: `Ofício nº ${configuration.oficioNumero}`,
+        text: `${configuration.oficioNumero}`,
         alignment: "right",
         fontSize: 12,
-        margin: [0, 0, 0, 20],
+        margin: [0, 10, 0, 20],
       },
       {
         text: `${configuration.oficioDestinatarioTratamento}\n${configuration.oficioDestinatarioCargo} ${configuration.oficioDestinatarioNome}\n${configuration.oficioDestinatarioInstituicao}`,
